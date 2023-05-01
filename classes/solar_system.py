b938b0da18d51b1899e8c16e2279b10481723f49
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2023-05-02 01:28:08 +0300

    Added a new class Planet to complement the SolarSystem class. Simplified the main.py file and some other parts.

--- classes/solar_system.py
+++ classes/solar_system.py
@@ -1,37 +1,63 @@
 from data.planet_data import planets
-from logic.sorting_dicts import sort_dict_by_value_itemgetter, sort_dict_masses_lambda
+from logic.sorting_dicts import sort_by_value_itemgetter, sort_dict_masses_lambda
+
+
+class Planet:
+    name: str
+    radius: int
+    mass: int
+    dist_from_sun: int
+    dist_from_earth: int
+
+    def __init__(
+        self,
+        name: str,
+        radius: int,
+        mass: int,
+        dist_from_sun: int,
+        dist_from_earth: int,
+    ):
+        self.name = name
+        self.radius = radius
+        self.mass = mass
+        self.dist_from_sun = dist_from_sun
+        self.dist_from_earth = dist_from_earth
 
 
 class SolarSystem:
-    planets: list[str]
+    planets: list[Planet]
     number_of_planets: int
-    radii: dict[str: int]
-    masses: dict[str: int]
-    distances_from_the_sun: dict[str: int]
-    distances_from_earth: dict[str: int]
+    radii: dict[str:int]
+    masses: dict[str:int]
+    dist_from_sun: dict[str:int]
+    dist_from_earth: dict[str:int]
 
     def __init__(self):
-        self.planets = list(planets.keys())
-        self.number_of_planets = len(self.planets)
+        list_of_planets = list()
 
+        for planet_name, planet_data in planets.items():
+            list_of_planets.append(Planet(*list(planet_data.values())))
+
+        self.planets = list_of_planets
+        self.number_of_planets = len(self.planets)
         self.radii = dict()
         self.masses = dict()
-        self.distances_from_the_sun = dict()
-        self.distances_from_earth = dict()
+        self.dist_from_sun = dict()
+        self.dist_from_earth = dict()
 
-        for planet_name in planets:
-            self.radii[planet_name] = planets[planet_name]["radius"]
-            self.masses[planet_name] = planets[planet_name]["mass"]
-            self.distances_from_the_sun[planet_name] = planets[planet_name]["distance from the sun"]
-            self.distances_from_earth[planet_name] = planets[planet_name]["distance from earth"]
+        for planet in self.planets:
+            self.radii[planet.name] = planet.radius
+            self.masses[planet.name] = planet.mass
+            self.dist_from_sun[planet.name] = planet.dist_from_sun
+            self.dist_from_earth[planet.name] = planet.dist_from_earth
 
     def sort_self_dicts(self):
-        copy_radii = sort_dict_by_value_itemgetter(self.radii, 1)
+        copy_radii = sort_by_value_itemgetter(self.radii, 1)
         copy_masses = sort_dict_masses_lambda(self.masses, 1)
-        copy_distances_from_the_sun = (sort_dict_by_value_itemgetter(self.distances_from_the_sun, 1))
-        copy_distances_from_earth = (sort_dict_by_value_itemgetter(self.distances_from_earth, 1))
+        copy_dist_from_sun = sort_by_value_itemgetter(self.dist_from_sun, 1)
+        copy_dist_from_earth = sort_by_value_itemgetter(self.dist_from_earth, 1)
 
         self.radii = copy_radii
         self.masses = copy_masses
-        self.distances_from_the_sun = copy_distances_from_the_sun
-        self.distances_from_earth = copy_distances_from_earth
+        self.dist_from_sun = copy_dist_from_sun
+        self.dist_from_earth = copy_dist_from_earth
--- src/main.py
+++ src/main.py
@@ -5,17 +5,7 @@ def main():
     ss = SolarSystem()
     ss.sort_self_dicts()
 
-    print(
-        "\n",
-        ss.radii,
-        "\n",
-        ss.masses,
-        "\n",
-        ss.distances_from_the_sun,
-        "\n",
-        ss.distances_from_earth,
-        "\n",
-    )
+    print(ss.dist_from_sun)
 
 
 if __name__ == "__main__":
